ba99a0e7ffc1f7a10f56df2055876dbc9fe12da0
tkfils <tkfils@users.noreply.github.com>
2018-01-04 15:17:49 +0000

    Add files via upload

---
+++ Koha download thing.bat
@@ -0,0 +1,6 @@
+cd "C:\users\nsmiton\Downloads"
+move cart.iso2709 "C:\Strawberry\perl\bin\"
+cd "C:\Strawberry\perl\bin\"
+perl -CS fanny_inkpen.pl > koha_output.txt
+del cart.iso2709
+notepad.exe "C:\Strawberry\perl\bin\koha_output.txt"
